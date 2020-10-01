c5d32b658a247bdbea959cab60a9bc5cc60bc18c
Vishnu Kakani <42287450+heeat@users.noreply.github.com>
2020-10-01 13:56:07 +0530

    gamwithsticks

---
+++ Codeforces-Div-2A/gamewithsticks.cpp
@@ -0,0 +1,36 @@
+// Codeforces Link : https://codeforces.com/problemset/problem/451/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+int main()
+{
+    int n = 0, m = 0, count = 0;
+
+    cin >> n >> m;
+
+    while (n * m)
+    {
+        n--;
+        m--;
+        count++;
+    }
+
+    if (count & 1)
+        cout << "Akshat";
+    else
+        cout << "Malvika";
+}
+
+// alternate solution
+// calculate minimum of both inputs and check if it is odd or even
+// if it is odd answer is Akshat because he has first move and his next move is 3, 5 ,7 ... odd numbers.
+
+/*
+
+    cin >> n >> m;
+    if ((min(n,m) & 1)) cout << "Akshat";
+    else cout << "Malvika";
+
+*/
